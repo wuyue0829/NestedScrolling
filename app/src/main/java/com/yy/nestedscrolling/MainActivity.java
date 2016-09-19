b915488542670c95fbc3ed6381675a193fabc904
wuyue <yy107829@sina.com>
2016-09-19 10:23:42 +0800

    Initial commit

---
+++ app/src/main/java/com/yy/nestedscrolling/MainActivity.java
@@ -0,0 +1,17 @@
+package com.yy.nestedscrolling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *  Android NestedScrolling机制完全解析
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
